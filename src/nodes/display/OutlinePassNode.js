2f640b038aab5fec77aa3ea47310e3a78483d4ef
Christian Helgeson <62450112+cmhhelgeson@users.noreply.github.com>
2024-09-17 10:35:21 -0700

    try to fix blending issue

--- src/nodes/display/OutlinePassNode.js
+++ src/nodes/display/OutlinePassNode.js
@@ -3,7 +3,7 @@ import QuadMesh from '../../renderers/common/QuadMesh';
 import { Vector2 } from '../../math/Vector2';
 import PassNode from './PassNode';
 import { uniform } from '../core/UniformNode.js';
-import { vec2, vec3, vec4, color, nodeObject, Fn } from '../tsl/TSLBase.js';
+import { vec2, vec3, vec4, color, nodeObject, Fn, mix } from '../tsl/TSLBase.js';
 import { Matrix4 } from '../../math/Matrix4.js';
 import { texture } from '../accessors/TextureNode.js';
 import { RenderTarget } from '../../core/RenderTarget.js';
@@ -42,8 +42,8 @@ class OutlinePassNode extends PassNode {
 		this.hiddenEdgeColor = uniformObject.hiddenEdgeColor || color( 0.1, 0.04, 0.02 );
 		this.edgeGlow = uniformObject.edgeGlow || 0.0;
 		this.usePatternTexture = uniformObject.usePatternTexture || 0;
-		this.edgeThickness = uniformObject.edgeThickness || 1.0;
-		this.edgeStrength = uniformObject.edgeStrength || 3.0;
+		this.edgeThickness = uniformObject.edgeThickness || uniform( 1.0 );
+		this.edgeStrength = uniformObject.edgeStrength || uniform( 3.0 );
 		this.downSampleRatio = uniformObject.downSampleRatio || 2;
 		this.pulsePeriod = uniformObject.pulsePeriod || 0;
 		this.blurIterations = uniformObject.blurIterations || 2;
@@ -288,19 +288,20 @@ class OutlinePassNode extends PassNode {
 		this.renderTarget.setSize( effectiveWidth, effectiveHeight );
 		this._nonSelectedRT.setSize( effectiveWidth, effectiveHeight );
 		this._selectedRT.setSize( effectiveWidth, effectiveHeight );
+		this._overlayRT.setSize( effectiveWidth, effectiveHeight );
 
 		let resx = Math.round( effectiveWidth / this.downSampleRatio );
 		let resy = Math.round( effectiveHeight / this.downSampleRatio );
 		this._maskDownSampleRT.setSize( resx, resy );
-		this._blur1RT.setSize( resx, resy );
-		this._edge1RT.setSize( resx, resy );
-		this._texSize.value.set( resx, resy );
+		//this._blur1RT.setSize( resx, resy );
+		//this._edge1RT.setSize( resx, resy );
+		//this._texSize.value.set( resx, resy );
 
 		resx = Math.round( resx / 2 );
 		resy = Math.round( resy / 2 );
 
-		this._blur2RT.setSize( resx, resy );
-		this._edge2RT.setSize( resx, resy );
+		//this._blur2RT.setSize( resx, resy );
+		//this._edge2RT.setSize( resx, resy );
 
 	}
 
@@ -336,12 +337,11 @@ class OutlinePassNode extends PassNode {
 
 		} );
 
+		const edgePass = gaussianBlur( sobel( this._downSampledColor.a ) ).renderOutput();
+
 		const overlay = Fn( () => {
 
-			const maskColor = this._selectedColor;
-			const visibilityFactor = maskColor.g.oneMinus();
-
-			return edgeStrength.mul( maskColor.r ).mul( edgeValue );
+			return edgePass.r;
 
 
 		} );
@@ -358,12 +358,6 @@ class OutlinePassNode extends PassNode {
 		overlayMaterial.depthTest = false,
 		overlayMaterial.depthWrite = false,
 		overlayMaterial.transparent = true;
-
-		const edgePass = gaussianBlur( sobel( this._downSampledColor.a ) );
-
-
-
-		return edgeStrength.mul( edgePass.r );
 
 	}
 
